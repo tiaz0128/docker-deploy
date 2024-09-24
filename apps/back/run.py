9818b519fb2424427cb1f349e82092251edc28a8
tiaz0128 <tiaz0128.dev@gmail.com>
2024-09-24 16:22:00 +0900

    add: Docker webhook

--- apps/back/run.py
+++ apps/back/run.py
@@ -1,7 +1,23 @@
+import logging
+import os
+import subprocess
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 def welcome_root():
     return {"message": "Hello FastAPI"}
+
+
+@app.post("/api/v1/containers/{container_id}/update")
+def update_container(container_id: str):
+    # 웹훅 수신 시 update_container.sh 스크립트 실행
+    logging.info(f"Update container: {container_id}")
+    script_path = os.path.join(
+        os.path.dirname(__file__), "scripts", "update_container.sh"
+    )
+    subprocess.call([script_path])
+    return "OK", 200
--- dockerfiles/front.Dockerfile
+++ dockerfiles/front.Dockerfile
@@ -14,7 +14,7 @@ FROM nginx:alpine
 
 COPY --from=build /app/build /usr/share/nginx/html
 
-COPY ./nginx/front-nginx.conf /etc/nginx/conf.d/default.conf
+COPY ./apps/nginx/front-nginx.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 8080
 
