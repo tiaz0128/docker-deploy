d593d7c434b2dc3bbd6fd158071b9699dc0aafcd
tiaz0128 <tiaz0128.dev@gmail.com>
2024-09-27 13:40:59 +0900

    add: buildspec.yaml

--- apps/scripts/update_container.sh
+++ apps/scripts/update_container.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 # 변수 설정
-IMAGE_NAME="your_dockerhub_username/your_image_name"
-CONTAINER_NAME="your_container_name"
+IMAGE_NAME="tiaz0128/fast-api"
+CONTAINER_NAME="fast-api"
 
 # 최신 이미지 pull
 docker pull $IMAGE_NAME:latest
 
 # 기존 컨테이너 중지 및 제거
 docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
 
 # 새 컨테이너 시작
-docker run -d --name $CONTAINER_NAME $IMAGE_NAME:latest
+docker run -d --rm --name $CONTAINER_NAME $IMAGE_NAME:latest
 
 echo "Container updated and restarted"
